ba904192bf7a4ab48c332de1a654c496294876c1
Xu Ren <musicalxu@hotmail.com>
2022-04-07 06:35:21 +0800

    init obj for problem 1 lru cache

---
+++ problem_1.py
@@ -0,0 +1,23 @@
+"""
+LRU Cache
+
+All operations must take O(1)
+
+An LRU cache is a type of cache in which we remove the least recently used entry when the cache memory reaches its limit. 
+For the current problem, consider both get and set operations as an use operation.
+"""
+
+
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        # Initialize class variables
+        pass
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent.
+        pass
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        pass
